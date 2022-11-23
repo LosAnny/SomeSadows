1626367491395b5cb724db5fa1a09a854d4d8589
los.anny <ann.losh99@mail.ru>
2022-11-23 21:37:49 +0300

    Initial Commit

---
+++ SomeSadows/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SomeSadows
+//
+//  Created by Анна Лошакова on 23.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
